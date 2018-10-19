85211526a58bb0a16900389bc6dddd7c2e430f8a
cembreyfarquhar <cembreyfarquhar@gmail.com>
2018-10-18 19:03:22 -0500

    working nav system

--- src/components/Header/Nav.js
+++ src/components/Header/Nav.js
@@ -5,86 +5,174 @@ class Nav extends React.Component {
   constructor() {
     super();
     this.state = {
-      navItems: [
-        {
-          name: "home",
-          clicked: false,
-          classes: "home"
-        },
-        {
-          name: "art",
-          clicked: false,
-          classes: "artExpanded"
-        },
-        {
-          name: "clothing",
-          clicked: false,
-          classes: "clothingExpanded"
-        },
-        {
-          name: "jewelry",
-          clicked: false,
-          classes: "jewelryExpanded"
-        },
-        {
-          name: "crafts",
-          clicked: false,
-          classes: "craftsExpanded"
-        }
-      ]
-    };
-    this.handleClick = this.handleClick.bind(this);
-    this.expandNav = this.expandNav.bind(this);
-    this.getClasses = this.getClasses.bind(this);
-  }
-
-  handleClick(ev) {
-    this.setState({
-      navItems: this.state.navItems.map(item => {
-        return (item.clicked = false);
-      })
-    });
-    const clickedText = ev.target.innerText;
-    const navItem = this.state.navItems.filter(item => {
-      return item.name === clickedText;
-    });
-    const unclickedItems = this.state.navItems.filter(item => {
-      return item.name !== clickedText;
-    });
-    this.state.navItems.forEach(item => {
-      this.getClasses(item.name);
-    })
-    navItem[0].clicked = true;
-    this.setState({
-      navItems: [...unclickedItems, navItem[0]]
-    });
-  }
-
-  expandNav() {
-  
-  }
-
-  getClasses(navItem) {
-    this.state.navItems.forEach(item => {
-      if(item.name === navItem && item.clicked === true) {
-        console.log(item)
-        item.classes = `${item.classes} expanded`
+      home: {
+        clicked: false,
+        class: ""
+      },
+      art: {
+        clicked: false,
+        class: ""
+      },
+      clothing: {
+        clicked: false,
+        class: ""
+      },
+      jewelry: {
+        clicked: false,
+        class: ""
+      },
+      crafts: {
+        clicked: false,
+        class: ""
       }
-    })    
+    };
+    this.expandArt = this.expandArt.bind(this);
+    this.expandClothing = this.expandClothing.bind(this);
+    this.expandJewelry = this.expandJewelry.bind(this);
+    this.expandCrafts = this.expandCrafts.bind(this);
+  }
+
+  expandArt() {
+    this.setState({
+      clothing: {
+        clicked: false,
+        class: ""
+      }, jewelry: {
+        clicked: false,
+        class: ""
+      }, crafts: {
+        clicked: false,
+        class: ""
+      }
+    });
+    if (this.state.art.clicked === false) {
+      this.setState({
+        art: {
+          clicked: true,
+          class: "expanded"
+        }
+      });
+    } else {
+      this.setState({
+        art: {
+          clicked: false,
+          class: ""
+        }
+      });
+    }
+  }
+
+  expandClothing() {
+    this.setState({
+      art: {
+        clicked: false,
+        class: ""
+      }, jewelry: {
+        clicked: false,
+        class: ""
+      }, crafts: {
+        clicked: false,
+        class: ""
+      }
+    });
+    if (this.state.clothing.clicked === false) {
+      this.setState({
+        clothing: {
+          clicked: true,
+          class: "expanded"
+        }
+      });
+    } else {
+      this.setState({
+        clothing: {
+          clicked: false,
+          class: ""
+        }
+      });
+    }
+  }
+
+  expandJewelry() {
+    this.setState({
+      clothing: {
+        clicked: false,
+        class: ""
+      }, art: {
+        clicked: false,
+        class: ""
+      }, crafts: {
+        clicked: false,
+        class: ""
+      }
+    });
+    if (this.state.jewelry.clicked === false) {
+      this.setState({
+        jewelry: {
+          clicked: true,
+          class: "expanded"
+        }
+      });
+    } else {
+      this.setState({
+        jewelry: {
+          clicked: false,
+          class: ""
+        }
+      });
+    }
+  }
+
+  expandCrafts() {
+    this.setState({
+      clothing: {
+        clicked: false,
+        class: ""
+      }, jewelry: {
+        clicked: false,
+        class: ""
+      }, art: {
+        clicked: false,
+        class: ""
+      }
+    });
+    if (this.state.crafts.clicked === false) {
+      this.setState({
+        crafts: {
+          clicked: true,
+          class: "expanded"
+        }
+      });
+    } else {
+      this.setState({
+        crafts: {
+          clicked: false,
+          class: ""
+        }
+      });
+    }
   }
 
   render() {
     return (
       <div className="navigation">
         <nav className="nav">
-          <span onMouseOver={this.handleClick}>home</span>
-          <span onClick={this.handleClick}>art</span>
-          <span onClick={this.handleClick}>clothing</span>
-          <span onClick={this.handleClick}>jewelry</span>
-          <span onClick={this.handleClick}>crafts</span>
+          <span>home</span>
+          <span onMouseOver={this.expandArt}>art</span>
+          <span onMouseOver={this.expandClothing}>clothing</span>
+          <span onMouseOver={this.expandJewelry}>jewelry</span>
+          <span onMouseOver={this.expandCrafts}>crafts</span>
         </nav>
-        <div className={this.state.navItems[1].classes}>
-          <h2>Art stuff :)</h2>
+        <div onMouseOut={this.expandArt} className={`artExpanded ${this.state.art.class}`}>
+          <h2>Art stuff</h2>
+        </div>
+        <div onMouseOut={this.expandClothing} className={`clothingExpanded ${this.state.clothing.class}`}>
+          <h2>clothes</h2>
+        </div>
+        <div onMouseOut={this.expandJewelry} className={`jewelryExpanded ${this.state.jewelry.class}`}>
+          <h2>Jewels</h2>
+        </div>
+        <div onMouseOut={this.expandCrafts} className={`craftsExpanded ${this.state.crafts.class}`}>
+          <h2>Crafts :)</h2>
         </div>
       </div>
     );
